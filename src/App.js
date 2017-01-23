544dce810f7be11baddfe92415c9b1a4f3be4e6b
Seve <sdkim@thoughtworks.com>
2017-01-23 17:14:09 -0600

    Created react template app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2> The QA Field Guide </h2>
+        </div>
+        <p> Coming Soon! </p>
+      </div>
+    );
+  }
+}
+
+export default App;
